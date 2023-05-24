4d374d38f4ad42a8c110a55485f58eae31d3d55f
Replit user <>
2023-05-24 18:52:57 +0000

    checkpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -33,6 +33,11 @@ JOBS = [
 @app.route("/")
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='Alex')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
