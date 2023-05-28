5bd92194d33c185816a60a8babaa6886da634a73
Replit user <>
2023-05-28 00:26:23 +0000

    add orderlist for jobs page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Jobstill')
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
@@ -19,7 +19,10 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
-  return jsonify(job)
+  if not job:
+    return "No posting found", 404
+
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
