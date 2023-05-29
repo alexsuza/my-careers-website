249863550b04fa6eb2604147aa9cee25ef4956b3
Replit user <>
2023-05-29 01:45:46 +0000

    fixed database endpoints

--- database.py
+++ database.py
@@ -28,6 +28,17 @@ def load_job_from_db(id):
     return dict(rows[0])
 
 
-def app_application_to_id(id):
+def add_application_to_db(job_id, data):
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs where id = :val"), val=id)
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+
+    conn.execute(query,
+                 job_id=job_id,
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 education=data['education'],
+                 work_experience=data['work_experience'],
+                 resume_url=data['resume_url'],
+                 linkedin_url=data['linkedin_url'])
